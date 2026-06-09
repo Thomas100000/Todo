4e682f131cfad5a9dc19895c414a7e75ffa5ae2c
Thomas <thomasjob500@gmail.com>
2026-06-09 13:27:37 +0530

    Fix database settings and decouple dependency error on Render

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('SUPABASE_DB_NAME', 'postgres'),
+        'USER': os.getenv('SUPABASE_DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('SUPABASE_DB_PASSWORD'),
+        'HOST': os.getenv('SUPABASE_DB_HOST', 'db.wufsktrtrkphulvpvwhp.supabase.co'),
+        'PORT': os.getenv('SUPABASE_DB_PORT', '5432'),
     }
 }
 
